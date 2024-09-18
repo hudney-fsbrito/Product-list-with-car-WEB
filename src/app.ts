0f6139c8f5385884b72052f307e9578bf4b4e5c6
hudney-fsbrito <hudney.rse@gmail.com>
2024-09-18 10:19:43 -0300

    refatora arquivos

---
+++ src/app.ts
@@ -0,0 +1,7 @@
+import data  from "../data.json"
+
+
+for (let i = 0; i < data.length; i++) {
+    // const element = array[i];
+    console.log(data[i].name);
+}
--- src/entities/products.ts
+++ src/entities/products.ts
@@ -1,18 +1,17 @@
+import { v4 as uuidv4 } from 'uuid';
 class Product{
-    private _id: string;
+    private _id: string = uuidv4();
     private _nameProduto: string;
     private _price: number;
     private _category: string;
     private _imageProdutoUrl:string;
 
     constructor (
-        id:string,
         nameProduto: string,
         price: number,
         category: string,
         imageProdutoUrl:string,
     ){
-        this._id = id;
         this._nameProduto = nameProduto;
         this._price = price;
         this._category = category;
