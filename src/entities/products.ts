d56372ad0e0b4ef7e6d3499f828b793fab7b7333
hudney-fsbrito <hudney.rse@gmail.com>
2024-09-23 08:02:15 -0300

    feat: Adiciona carrinho de compras (em andamento)

--- src/entities/products.ts
+++ src/entities/products.ts
@@ -1,22 +1,30 @@
 import { v4 as uuidv4 } from 'uuid';
 
 export class Product{
+    [x: string]: any;
     private _id: string = uuidv4();
-    private _nameProduto: string;
+    private _nameProduct: string;
     private _price: number;
     private _category: string;
-    private _imageProdutoUrl:string;
+    private _imageProductUrl:string;
 
     constructor (
-        nameProduto: string,
+        nameProduct: string,
         price: number,
         category: string,
-        imageProdutoUrl:string,
+        imageProductUrl:string,
     ){
-        this._nameProduto = nameProduto;
+        this._nameProduct = nameProduct;
         this._price = price;
         this._category = category;
-        this._imageProdutoUrl = imageProdutoUrl;
+        this._imageProductUrl = imageProductUrl;
+    }
+    
+    get getNameProduct(){
+        return this._nameProduct
+    }
+    get getPrice(){
+        return this._price
     }
 
 }
